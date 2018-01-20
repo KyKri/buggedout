674ef3b9ed5ebcee50b8da7724ddec7979f49ee4
Kyle Krick <krkrick@gmail.com>
2018-01-19 21:00:05 -0800

    added firebase api constants

---
+++ app/core/constant/constants.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBtLFsCz5v8bxgmIew2B_mxXG2ke0OVKWI",
+    authDomain: "bugged-out-770a3.firebaseapp.com",
+    databaseURL: "https://bugged-out-770a3.firebaseio.com",
+    projectId: "bugged-out-770a3",
+    storageBucket: "bugged-out-770a3.appspot.com",
+    messagingSenderId: "491653019150"
+};
+//# sourceMappingURL=constants.js.map
---
+++ app/core/constant/constants.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBtLFsCz5v8bxgmIew2B_mxXG2ke0OVKWI",
+    authDomain: "bugged-out-770a3.firebaseapp.com",
+    databaseURL: "https://bugged-out-770a3.firebaseio.com",
+    projectId: "bugged-out-770a3",
+    storageBucket: "bugged-out-770a3.appspot.com",
+    messagingSenderId: "491653019150"
+}
